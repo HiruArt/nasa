8de7d8090883aa73dc455f9a0c57ad2fe4c85fd6
DarthJktu <DarthJktu@gmail.com>
2019-12-10 14:30:06 +0200

    топ

--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
 $(document).ready(function () {
+    // var scene = document.getElementById('scene');
+    // var parallax = new Parallax(scene);
+    //
+    // document.getElementById('deleteme').onclick = function() {
+    //     parallax.destroy();
+    //     parallax = null;
+    //
+    //     this.onclick = null;
+    //     this.style.display = 'none';
+    // };
 
+    // $('html,body').animate({
+    //     scrollTop: $(".second-block").offset().top
+    // });
+
+    var blocks = [];
+    var currentBlock = 0;
+    $('.scroll-block').each(function () {
+        blocks.push($(this).attr('id'));
+    });
+
+    console.log(blocks);
+
+    if(location.hash != ''){
+        var hash = location.hash;
+        var blockName = '#'+hash;
+
+
+
+        $('html,body').animate({
+            scrollTop: $(blockName).offset().top
+        });
+    }
+
+    $(window).on('mousewheel', function(e) {
+        var delta = e.originalEvent.deltaY;
+        console.log(delta);
+        if (delta == 100){
+            location.hash = '';
+        }
+        // return false;
+        if(location.hash == ''){
+        }
+    });
+
+    $(document).on('scroll', function (e) {
+
+    })
 });
 
 
