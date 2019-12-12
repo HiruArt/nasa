2e2285c47c07fbd8d34102539610f66403258fa8
ARTEM <girukartem@gmail.com>
2019-12-12 15:59:16 +0200

    qwe

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
+    if(window.innerWidth > 600) {
+        var scene = document.getElementsByClassName('paralax-1')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-1')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-2')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-2')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-3')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-3')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-4')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-4')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-5')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-5')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-6')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-6')[0];
-    var parallax = new Parallax(scene);
+        var scene = document.getElementsByClassName('paralax-7')[0];
+        var parallax = new Parallax(scene);
 
-    var scene = document.getElementsByClassName('paralax-7')[0];
-    var parallax = new Parallax(scene);
 
+        var rellax = new Rellax('.rellax');
+    }
     AOS.init();
 
-    var rellax = new Rellax('.rellax');
 
     // document.getElementById('deleteme').onclick = function() {
     //     parallax.destroy();
@@ -227,39 +229,39 @@ $(document).ready(function () {
         //
         // });
     }
+    if(window.innerWidth > 600) {
+        var stars = $('.star-solo').length;
+        var comets = $('.comet').length;
 
-    var stars = $('.star-solo').length;
-    var comets = $('.comet').length;
+        setInterval(function () {
+            var random = randomInteger(0, stars);
 
-    setInterval(function () {
-        var random = randomInteger(0, stars);
+            $('.star-solo').eq(random).addClass('animate');
 
-        $('.star-solo').eq(random).addClass('animate');
+            setTimeout(function () {
+                $('.star-solo').eq(random).removeClass('animate');
+            }, 1000);
 
-        setTimeout(function () {
-            $('.star-solo').eq(random).removeClass('animate');
-        }, 1000);
-
-    }, 5000);
+        }, 5000);
 
 
-    setInterval(function () {
-        var random = randomInteger(0, stars);
+        setInterval(function () {
+            var random = randomInteger(0, stars);
 
-        $('.comet').eq(random).addClass('animate');
+            $('.comet').eq(random).addClass('animate');
 
-        setTimeout(function () {
-            $('.comet').eq(random).removeClass('animate');
-        }, 7000);
+            setTimeout(function () {
+                $('.comet').eq(random).removeClass('animate');
+            }, 7000);
 
-    }, 5000);
+        }, 5000);
 
-    function randomInteger(min, max) {
-        // случайное число от min до (max+1)
-        let rand = min + Math.random() * (max + 1 - min);
-        return Math.floor(rand);
+        function randomInteger(min, max) {
+            // случайное число от min до (max+1)
+            let rand = min + Math.random() * (max + 1 - min);
+            return Math.floor(rand);
+        }
     }
-
 
 
 });
