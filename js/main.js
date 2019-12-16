cbee518e92dc2ef75a884ba8e0340958e01f3215
DarthJktu <DarthJktu@gmail.com>
2019-12-16 14:19:55 +0200

    Merge branch 'master' of https://github.com/HiruArt/nasa
    # Conflicts:
    #	css/style.css

--- js/main.js
+++ js/main.js
@@ -27,16 +27,6 @@ $(document).ready(function () {
     }
     AOS.init();
 
-
-    // document.getElementById('deleteme').onclick = function() {
-    //     parallax.destroy();
-    //     parallax = null;
-    //
-    //     this.onclick = null;
-    //     this.style.display = 'none';
-    // };
-
-
     var ts;
     $(window).on('touchstart', function (e) {
         ts = $(document).scrollTop();
@@ -47,28 +37,7 @@ $(document).ready(function () {
         var animTop = false;
         flagScroll = false;
 
-        // $(window).on('touchmove mousewheel wheel', function (e) {
-        //
-        //     let scroll = this.pageYOffset;
-        //
-        //     var secondBlock = $('.second-block').offset().top;
-        //
-        //     var delta = e.originalEvent.deltaY;
-        //
-        //
-        //     console.log(scroll);
-        //     console.log( $(this).scrollTop());
-        //     console.log(this.scrollY);
-        //     $('.top-rocket').css('transform', 'translate(-'+scroll/2+'%, -'+scroll/4+'%) rotate(-34.5deg)');
-        //
-        //
-        //
-        //     flagScroll = true;
-        //     animTop = true;
-        //
-        //
-        //
-        // });
+
         var nScroll = 10;
         $(window).scroll(function () {
             let scroll = $(this).scrollTop();
@@ -94,250 +63,15 @@ $(document).ready(function () {
             }
 
 
-            // if(window.innerWidth < 1600 ) {
+
                 $('.top-block .inner-block').css('opacity', 1 - scroll / 550);
 
-            // }
+
 
         });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // $(window).on('touchmove mousewheel wheel', function (e) {
-        //
-        //     if (flagScroll) return;
-        //
-        //     let height = this.innerHeight;
-        //     let scroll = this.pageYOffset;
-        //     let scrollDelta = e.originalEvent.deltaY;
-        //
-        //     var secondBlock = $('.second-block').offset().top;
-        //
-        //     var delta = e.originalEvent.deltaY;
-        //     // console.log(height);
-        //     // console.log(scroll);
-        //     // console.log(scrollDelta);
-        //     // console.log(secondBlock);
-        //
-        //
-        //     if (delta > 0 && animTop == false){
-        //
-        //
-        //         flagScroll = true;
-        //         animTop = true;
-        //
-        //         $('.page-wrap').addClass('top-animate');
-        //         $('.page-wrap').addClass('active ');
-        //
-        //         setTimeout(function () {
-        //
-        //             $('body').addClass('page--scroll');
-        //             $('html,body').animate({
-        //                 scrollTop: $('.second-block').offset().top
-        //             }, 900, function () {
-        //             });
-        //         },600);
-        //
-        //         setTimeout(function () {
-        //             $('.page-wrap').removeClass('active ');
-        //         }, 1500);
-        //
-        //         setTimeout(function () {
-        //             $('.page-wrap').removeClass('top-animate');
-        //             $('.page-wrap').addClass('top');
-        //             flagScroll = false;
-        //         }, 2000);
-        //
-        //         setTimeout(function () {
-        //             $('.page-wrap').removeClass('top');
-        //         }, 2200);
-        //
-        //
-        //
-        //         // }
-        //
-        //     } else {
-        //         if(scroll < secondBlock - 100 && flagScroll == false){
-        //             console.log(222);
-        //             animTop = false;
-        //             flagScroll = true;
-        //             $('html,body').animate({
-        //                 scrollTop: 0
-        //             }, 900, function () {
-        //
-        //                 $('body').removeClass('page--scroll');
-        //                 $('.page-wrap').addClass('top-show');
-        //                 $('.page-wrap').removeClass('top');
-        //                 $('.page-wrap').removeClass('active ');
-        //                 setTimeout(function () {
-        //
-        //
-        //                 },500);
-        //                 setTimeout(function () {
-        //                     $('.page-wrap').removeClass('top-show');
-        //                     flagScroll = false;
-        //                 },1000);
-        //
-        //             });
-        //
-        //
-        //         }
-        //         // if (scroll == 0) {
-        //         //     flagScroll = true;
-        //         //     setTimeout(function () {
-        //         //         flagScroll = false;
-        //         //     }, 500);
-        //         //
-        //         //     $('.full-block').removeClass('hide-top');
-        //         //     $('header').removeClass('fixed');
-        //         //     $('body').addClass('page--hidden');
-        //         // }
-        //
-        //     }
-        //
-        //
-        // });
-        //
-        //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // var blocks = [];
-        // var currentBlock = 0;
-        // var scrollingFlag = false;
-        // $('.scroll-block').each(function () {
-        //     blocks.push($(this).attr('data-id'));
-        // });
-
-
-        // if(location.hash != ''){
-        //     var hash = location.hash.split('#')[1];
-        //
-        //     blocks.forEach(function (name, index) {
-        //         if(name == hash){
-        //             currentBlock = index;
-        //         }
-        //     });
-        //     scrollingFlag = true;
-        //     setTimeout(function () {
-        //         $('html,body').animate({
-        //             scrollTop: $('[data-id="'+hash+'"]').offset().top
-        //         }, 900, function () {
-        //             scrollingFlag = false;
-        //         });
-        //     },300);
-        //
-        // }
-
-        // $(window).on('mousewheel', function(e) {
-        //     if (scrollingFlag){
-        //         return;
-        //     }
-        //
-        //     var delta = e.originalEvent.deltaY;
-        //     var top = 50;
-        //     console.log(delta);
-        //
-        //     if (delta > 0){
-        //         if(currentBlock != blocks.length - 1){
-        //             currentBlock = currentBlock + 1;
-        //             scrollingFlag = true;
-        //             var t = 0;
-        //
-        //             if(currentBlock == 1){
-        //                 $('.top-block').addClass('active');
-        //                 t=1100;
-        //             }
-        //
-        //             if($('[data-id="'+blocks[currentBlock]+'"]').attr('data-pixel')){
-        //                 top = $('[data-id="'+blocks[currentBlock]+'"]').attr('data-pixel')
-        //             }
-        //
-        //             setTimeout(function () {
-        //
-        //
-        //                 console.log(currentBlock);
-        //                 $('html,body').animate({
-        //                     scrollTop: $('[data-id="'+blocks[currentBlock]+'"]').offset().top - top
-        //                 }, 900, function () {
-        //                     scrollingFlag = false;
-        //                     $('.top-block').removeClass('active');
-        //                 });
-        //             }, t);
-        //             location.hash = blocks[currentBlock];
-        //         }
-        //
-        //
-        //
-        //
-        //     }
-        //
-        //     else{
-        //         if(currentBlock != 0){
-        //             currentBlock = currentBlock - 1;
-        //             scrollingFlag = true;
-        //
-        //             if($('[data-id="'+blocks[currentBlock]+'"]').attr('data-pixel')){
-        //                 top = $('[data-id="'+blocks[currentBlock]+'"]').attr('data-pixel')
-        //             }
-        //
-        //             $('html,body').animate({
-        //                 scrollTop:  $('[data-id="'+blocks[currentBlock]+'"]').offset().top - top
-        //             }, 900, function () {
-        //                 scrollingFlag = false;
-        //             });
-        //
-        //             location.hash = blocks[currentBlock];
-        //         }
-        //     }
-        //
-        //
-        // });
     }
     if(window.innerWidth > 600) {
         var stars = $('.star-solo').length;
